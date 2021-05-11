e69dfbf4da0e1de73d6b4833de1dea3fba6e3393
andy.sg <andy@kakaopage.com>
2021-05-11 22:55:30 +0900

    Deploy website - based on 6954ef393af84d9742baab748f59b1a391f04413

---
+++ assets/js/546523cb.34d09b8d.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjustpython_doc=self.webpackChunkjustpython_doc||[]).push([[158],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ assets/js/b2b675dd.08b88a5e.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjustpython_doc=self.webpackChunkjustpython_doc||[]).push([[533],{2383:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
